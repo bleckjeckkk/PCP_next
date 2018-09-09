57a90211d8f4e237a3d974ef6a72adfae04a7f66
Dan <danjoaquinchin@gmail.com>
2018-09-10 01:32:36 +0800

    just don't refresh pls haha

--- components/Layout.js
+++ components/Layout.js
@@ -10,6 +10,17 @@ const Layout = (props) => (
   <div style={{ margin : '-8px'}}>
     <Head>
       <title>PCP {props.page ? `| ${props.page}` : ''}</title>
+      <meta charSet="utf-8" />
+      {/* Use minimum-scale=1 to enable GPU rasterization */}
+      <meta
+        name="viewport"
+        content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+      />
+      {/* PWA primary color */}
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
+      />
     </Head>
     <Header user={props.user}/>
     <div style={layoutStyle}>
