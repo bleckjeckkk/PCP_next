f1cd98e5bbdc5b2cd574ebc72ba088600d1a62e3
Doodyo123 <GamemasterD@users.noreply.github.com>
2018-09-10 15:51:18 +0800

    useraccount update

--- pages/userAccount.js
+++ pages/userAccount.js
@@ -10,6 +10,7 @@ import { Typography, Grid, Paper, Snackbar } from '@material-ui/core'
 import Router from 'next/router'
 import { PCP_SERVER } from '../res/ImportantThings'
 
+import FormWrapper from '../components/Form'
 import SnackbarWrapper from '../components/Snackbar'
 
 const CHAR_LIMIT = 120;
@@ -20,11 +21,13 @@ class Index extends Component {
     constructor(props){
         super(props)
         this.state = {
+            feedback : '',
             text : '',
             user : { user : {}},
             snackbarMode : '',
             snackbarMessage : '',
             open : false,
+            formOpen : false
         };
     }
 
@@ -37,6 +40,7 @@ class Index extends Component {
         }
         this.getMaxID();
         this.setState({ user });
+        console.log({user});
     }
 
     getMaxID(){
@@ -53,11 +57,45 @@ class Index extends Component {
         localStorage.setItem('key', this.state.text);
     }
 
+    onFeedbackChange(event){
+        this.setState({
+            feedback : event.target.value,
+        });
+    }
+
     onChange(event){
         this.setState({
             text : event.target.value,
         });
     }
+
+    handleFormOpen = (userUpdate, value) => {
+      console.log({userUpdate, value});
+      switch(userUpdate){
+        case 'firstName': this.setState({
+                                        formOpen : true,
+                                        text : value,
+                                        attribute : userUpdate
+                                        });
+                                        break;
+        case 'lastName': this.setState({
+                                        formOpen : true,
+                                        text : value,
+                                        attribute : userUpdate
+                                      });
+                                      break;
+        case 'username': this.setState({
+                                        formOpen : true,
+                                        text : value,
+                                        attribute : userUpdate
+                                      });
+                                      break;
+      }
+    };
+
+    handleFormClose = () => {
+        this.setState({ formOpen: false });
+    };
 
     logout(){
         Router.push('/login')
@@ -66,26 +104,79 @@ class Index extends Component {
         });
     }
 
+    updateUser(){
+        this.showSnackbar('info','Updating entry...');
+        this.handleFormClose();
+        const {user} = this.state.user;
+        console.log({user});
+        var u = {
+          id : user.id
+        };
+        switch(this.state.attribute){
+          case 'firstName': u = {
+                                              id:user.id,
+                                              firstName : this.state.text,
+                                              lastName : user.lastName,
+                                              username: user.userName
+                                            }
+                                          break;
+          case 'lastName': u = {
+                                              id:user.id,
+                                              firstName : user.firstName,
+                                              lastName : this.state.text,
+                                              username: user.userName
+                                            }
+                                          break;
+          case 'username': u = {
+                                              id:user.id,
+                                              firstName : user.firstName,
+                                              lastName : user.lastName,
+                                              username: this.state.text
+                                            }
+                                          break;
+        }
+        console.log({u});
+        fetch(`${PCP_SERVER}/users/update?userID=${u.id}&firstName=${u.firstName}&lastName=${u.lastName}&userName=${u.username}`)
+        .then(response => response.json())
+        .then(response => {
+          console.log({response});
+          if(response.msg == 'success'){
+            this.showSnackbar('success', 'Entry updated');
+          }else{
+            this.showSnackbar('error','An error occured. Please try again.');
+            console.error(response.res);
+          }
+        })
+        .catch((e)=> console.log(e))
+        this.setState({text : ''});
+        switch(this.state.attribute){
+          case 'username' : this.logout();
+                                         break;
+          default : break;
+        }
+    }
+
     sendFeedback(){
-        if(this.state.text.length > CHAR_LIMIT){
+        if(this.state.feedback.length > CHAR_LIMIT){
             this.showSnackbar('error', `Message exceeded ${CHAR_LIMIT} characters!`);
             return;
         }
         this.showSnackbar('info', 'Sending Feedback');
-        fetch(`${PCP_SERVER}/feedbacks/add?feedbackID=${this.state.lastFeedbackID}&userID=${this.state.user.user.id}&productName=${this.state.text}&productID=0
+        fetch(`${PCP_SERVER}/feedbacks/add?feedbackID=${this.state.lastFeedbackID}&userID=${this.state.user.user.id}&productName=${this.state.feedback}&productID=0
         `)
         .then(response => response.json())
         .then(response => {
             if(response.msg == 'success'){
                 this.showSnackbar('success', 'Feedback sent! Thank you.');
-                this.setState({ text : '' });
+                this.setState({ feedback : '' });
             }else{
                 this.showSnackbar('error', `Error ${response.res.code}`);
+                this.setState({ feedback : '' });
             }
             console.log(response);
             this.getMaxID();
         })
-        .catch(err => {console.log(err);}) 
+        .catch(err => {console.log(err);})
 
     }
 
@@ -95,7 +186,7 @@ class Index extends Component {
             mode,
             key: new Date().getTime(),
         });
-    
+
         if (this.state.open) {
           this.setState({ open: false });
         } else {
@@ -169,9 +260,9 @@ class Index extends Component {
                                             alignItems="center"
                                             style={{ padding : 20 }}
                                         >
-                                            <Button variant="contained">{this.state.user.user.firstName}</Button> 
-                                            <Button variant="contained">{this.state.user.user.lastName}</Button> 
-                                            <Button variant="contained">{this.state.user.user.userName}</Button>  
+                                            <Button variant="contained" onClick={() => this.handleFormOpen('firstName', this.state.user.user.firstName)}>{this.state.user.user.firstName}</Button>
+                                            <Button variant="contained" onClick={() => this.handleFormOpen('lastName', this.state.user.user.lastName)}>{this.state.user.user.lastName}</Button>
+                                            <Button variant="contained" onClick={() => this.handleFormOpen('username', this.state.user.user.userName)}>{this.state.user.user.userName}</Button>
                                             <Button variant="contained" color="secondary" onClick={() => this.logout()}> Log Out </Button>
                                         </Grid>
                                     </Grid>
@@ -202,7 +293,7 @@ class Index extends Component {
                                         TABLE HERE
                                     </div>
                                     </Typography>
-                                    <Button variant="contained">Compare</Button> 
+                                    <Button variant="contained">Compare</Button>
                                 </Grid>
                             </Paper>
                         </Grid>
@@ -220,15 +311,15 @@ class Index extends Component {
                                         FEEDBACK
                                     </Typography>
                                     <TextField
-                                        error={this.state.text.length > CHAR_LIMIT}
+                                        error={this.state.feedback.length > CHAR_LIMIT}
                                         id="feedbackText"
-                                        value={this.state.text}
-                                        onChange={this.onChange.bind(this)}
+                                        value={this.state.feedback}
+                                        onChange={this.onFeedbackChange.bind(this)}
                                         multiline
                                         rowsMax="8"
                                         margin="normal"
                                         fullwidth
-                                        helperText={`${this.state.text.length}/${CHAR_LIMIT}`}
+                                        helperText={`${this.state.feedback.length}/${CHAR_LIMIT}`}
                                     />
                                     <Button variant="contained" onClick={() => this.sendFeedback()}>SEND FEEDBACK</Button>
                                 </Grid>
@@ -237,6 +328,25 @@ class Index extends Component {
                     </Grid>
                 </Grid>
             </Grid>
+
+            <FormWrapper
+                isOpen={this.state.formOpen}
+                onClose={this.handleFormClose}
+                title='user Form'
+                subtitle={`Enter new ${this.state.attribute}`}
+                acceptFunction= {() => this.updateUser()}
+                acceptText= "Update"
+                >
+                <TextField
+                    margin="dense"
+                    id="txtBox"
+                    label={this.state.attribute}
+                    fullWidth
+                    onChange={this.onChange.bind(this)}
+                    value={this.state.text}
+                />
+            </FormWrapper>
+
             <Snackbar
                 anchorOrigin={{
                     vertical: 'bottom',
