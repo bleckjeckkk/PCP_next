d8d2695ebbaebafccc9656da21bc95a46eb90fb9
bleckjeckkk <danjoaquinchin@gmail.com>
2018-11-04 20:12:48 +0800

    pinaka-last push.
    updated ImportantThings to use the online PCP "API"

--- res/ImportantThings.js
+++ res/ImportantThings.js
@@ -1,5 +1,8 @@
 // NON-LOCALHOST
-//export const PCP_SERVER = 'http://' + '192.168.1.1' + ':4000'
+// export const PCP_SERVER = 'http://' + '192.168.1.1' + ':4000'
 
 // LOCALHOST
-export const PCP_SERVER = 'http://' + 'localhost' + ':4000'
+// export const PCP_SERVER = 'http://' + 'localhost' + ':4000'
+
+// ONLINE SERVER OMG
+export const PCP_SERVER = 'https://' + 'pcp-express.herokuapp.com'
